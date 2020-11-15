4d0b97026fd778b6652e6c3bf4a8b291107fdc87
Stephany Bolivar <g.stephanybolivar@gmail.com>
2020-11-15 16:10:16 -0500

    Set view route for index.html

--- routes/routes.js
+++ routes/routes.js
@@ -18,11 +18,14 @@ module.exports = (app) => {
     app.get("/notes", (req, res) => {
       res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
   });
 };
 
 //   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "./public/index.html"));
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
 //   });
 
 // };
