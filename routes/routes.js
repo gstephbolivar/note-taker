16250e01dcbab2722a9bd5cf50a1b1b1aaae42cf
Stephany Bolivar <g.stephanybolivar@gmail.com>
2020-11-16 15:46:18 -0500

    [WIP] Deleting the correct note

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,11 @@
 const path = require("path");
 const fs = require("fs");
+// const {v4: uuidv4} = require("uuid");
 
 module.exports = (app) => {
   fs.readFile("db/db.json", "utf8", (err, data) => {
     if (err) throw err;
-    const notes = JSON.parse(data);
+    let notes = JSON.parse(data);
 
     // API Routes
     app.get("/api/notes", (req, res) => {
@@ -12,10 +13,21 @@ module.exports = (app) => {
     });
 
     app.post("/api/notes", (req, res) => {
-      const newNote = req.body;
+      let newNote = req.body;
+      console.log(newNote);
+    //   newNote.id = uuidv4();
       notes.push(newNote);
       updatedDb();
     });
+
+    app.get("/api/notes/:id", (req,res) => {
+        res.json(notes[req.params.id]);
+    })
+
+    app.delete("/api/notes/:id", (req, res) => {
+        notes.splice(req.params.id, 1);
+        updateDb(notes);
+    })
 
     // View Routes
     app.get("/notes", (req, res) => {
@@ -28,7 +40,7 @@ module.exports = (app) => {
 
     // Function to update db.json file whenever a note is added or deleted
     function updatedDb() {
-      fs.writeFile("db/db.json", "utf8", (err, data) => {
+      fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), (err, data) => {
         if (err) throw err;
         return true;
       });
