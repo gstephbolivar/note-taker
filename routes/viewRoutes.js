e73aeba8194d15f330ab8e281bb15884b2c22393
Stephany Bolivar <g.stephanybolivar@gmail.com>
2020-11-17 17:08:37 -0500

    Commented code

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,10 +2,12 @@ const path = require("path");
 
 module.exports = (app) => {
   // View Routes
+
+//   View the notes page to write,read, delete notes
   app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
-
+// If no matching route is found default to home
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
