1629f4d7b736201dfbfd55d296ac88e6e48d7e41
Stephany Bolivar <g.stephanybolivar@gmail.com>
2020-11-17 18:43:12 -0500

    Post route done. Installed util package and created a promise for when the new note is posted

--- routes/APIroutes.js
+++ routes/APIroutes.js
@@ -1,31 +1,28 @@
 const path = require("path");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
-let db = require("../db/db.json");
-const dbData = require("../data/notesData");
-const { writeFileSync } = require("fs");
+const db = require("../db/db.json");
+// const dbData = require("../data/notesData");
+const util = require("util");
+const writeFileSync = util.promisify(fs.writeFile);
 
 module.exports = (app) => {
   // API Routes
   app.get("/api/notes", (req, res) => {
     return res.json(db);
   });
-
+// Route to post a new note
   app.post("/api/notes", (req, res) => {
-    let newNote = req.body;
-    newNote.id = uuidv4();
-    let notes = fs.writeFileSync("../db/db.json", JSON.stringify(db))
-    .then(() => {
-      res.json(notes);
-    })
-    .catch((err) => console.error(err));
-  
-    // let notes = dbData.readNotes();
-    notes.push(newNote);
+    let note = req.body;
+    let id = uuidv4();
+    note.id = id;
+    writeFileSync("./db/db.json", JSON.stringify(db))
+      .then(() => {
+        res.json(note);
+      })
+      .catch((err) => console.error(err));
 
-    // dbData.writeNotes(notes);
-
-    // return res.json(notes);
+    db.push(note);
   });
 
   // app.delete("/api/notes/:id", (req, res) => {
