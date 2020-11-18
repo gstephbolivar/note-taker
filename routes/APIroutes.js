54de340205c98923c02295c418b8e83d7437453d
Stephany Bolivar <g.stephanybolivar@gmail.com>
2020-11-17 21:03:09 -0500

    [WIP] How to take a note off list once deleted

--- routes/APIroutes.js
+++ routes/APIroutes.js
@@ -2,7 +2,6 @@ const path = require("path");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 const db = require("../db/db.json");
-// const dbData = require("../data/notesData");
 const util = require("util");
 const writeFileSync = util.promisify(fs.writeFile);
 
@@ -11,21 +10,35 @@ module.exports = (app) => {
   app.get("/api/notes", (req, res) => {
     return res.json(db);
   });
-// Route to post a new note
+  // Route to post a new note
   app.post("/api/notes", (req, res) => {
+    // console.log(db);
     let note = req.body;
+    // console.log(note);
     let id = uuidv4();
     note.id = id;
+    // console.log(db);
+    db.push(note);
     writeFileSync("./db/db.json", JSON.stringify(db))
       .then(() => {
         res.json(note);
       })
       .catch((err) => console.error(err));
-
-    db.push(note);
   });
-
-  // app.delete("/api/notes/:id", (req, res) => {
-
-  // })
+  // Route to delete a note
+  app.delete("/api/notes/:id", (req, res) => {
+    // receive query parameter containing id of note to delete
+    let newNote = db.filter(note => note.id != req.params.id);
+    console.log(newNote);
+    const updateNote = newNote;
+    writeFileSync("./db/db.json", JSON.stringify(updateNote))
+      .then(() => {
+        res.json(updateNote);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    // each note needs a unique ID
+    // to delete, review all db.json file, remove note w/ id AND rewrite db.json file
+  });
 };
